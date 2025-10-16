c43138eaa7d419708f5b2b55216ceb2c155360ac
vineeth9997 <vineethrao.polneni@gmail.com>
2025-10-15 20:59:13 -0500

    second commit

---
+++ calculator.py
@@ -0,0 +1,3 @@
+def add_numbers(a, b):
+    """Return the sum of two numbers"""
+    return a - b   # <-- BUG: should be a + b
---
+++ test_calculator.py
@@ -0,0 +1,4 @@
+from calculator import add_numbers
+
+def test_add():
+    assert add_numbers(2, 3) == 5
